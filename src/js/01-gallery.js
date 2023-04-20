49385e6baf4e03a4471a6587fc563c498f1a77e3
OlegatorIT <podlesnyoleg93@gmail.com>
2023-04-21 01:19:02 +0200

    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function addImages() {
+  const addImg = galleryItems
+    .map(
+      el =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${el.original}">
+              <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+            </a>
+          </li>`
+    )
+    .join('');
+  galleryEl.innerHTML = addImg;
+}
+
+addImages();
+
+const options = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+new SimpleLightbox('.gallery__link', options);
