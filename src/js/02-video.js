406359f8a993cd6f3db21f0fe1851a450eacd112
OlegatorIT <podlesnyoleg93@gmail.com>
2023-04-23 04:00:22 +0200

    Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const saveTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(saveTime);
+
+const savedVolume = JSON.parse(localStorage.getItem('volume'));
+if (savedVolume !== null) {
+  player.setVolume(savedVolume.volume);
+}
+
+player.on('volumechange', volume => {
+  localStorage.setItem('volume', JSON.stringify(volume));
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+const { email, message } = feedbackForm.elements;
+
+const localStorageObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+chackedLocalStorage();
+
+feedbackForm.addEventListener('input', throttle(onValueInput, 500));
+feedbackForm.addEventListener('submit', onSubmit);
+
+function onValueInput() {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentValuesObj()));
+}
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(currentValuesObj());
+  feedbackForm.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function chackedLocalStorage() {
+  if (localStorageObj === null) {
+    return;
+  }
+  if (localStorageObj.message || localStorageObj.email !== '') {
+    email.value = localStorageObj.email;
+    message.value = localStorageObj.message;
+  }
+}
+
+function currentValuesObj() {
+  return {
+    email: email.value,
+    message: message.value,
+  };
+}
